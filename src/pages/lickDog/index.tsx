ddf380e169f17e75a5f7aa6b536aaefce7cc4e51
王瑞江 <wangrj@weipaitang.com>
2021-03-19 18:08:00 +0800

    feat: init

---
+++ src/pages/lickDog/index.tsx
@@ -0,0 +1,15 @@
+// https://v1.alapi.cn/api/dog?format=text
+import React from 'react'
+
+export default class LickDog extends React.PureComponent {
+  state = {
+
+  }
+
+  render() {
+    console.log('dog')
+    return (
+      <div>123</div>
+    )
+  }
+}
